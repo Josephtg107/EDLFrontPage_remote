db212e43c42c5aaa4b7314362a285e4705a777c9
Joseph <josephtg107@gmail.com>
2023-03-24 08:30:56 -0500

    Initial Commit

---
+++ EDLFrontPage/EDLFrontPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  EDLFrontPageApp.swift
+//  EDLFrontPage
+//
+//  Created by Joseph Garcia on 24/03/23.
+//
+
+import SwiftUI
+
+@main
+struct EDLFrontPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
